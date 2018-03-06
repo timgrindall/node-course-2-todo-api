b56235584e33f3ecd6327059b50dd900cc98fe39
Timothy Grindall <tim.grindall@gmail.com>
2018-03-06 11:04:06 -0800

    add delete and update scripts

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+	const db = client.db('TodoApp');
+
+	// deleteMany
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// })
+
+	// fineOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// delete many occurances of 'Timothy'
+	// db.collection('Users').deleteMany({name: 'Timothy'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// delete one by _id
+	db.collection('Users').findOneAndDelete({_id: new ObjectID("5a9ddd7f3513b7557ac6c1ef")}).then((result) => {
+		console.log(result);
+	});
+
+	client.close();
+});
