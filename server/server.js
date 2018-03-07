743cdceae3c8bb09cf87355e30290c5f4ede51b9
Timothy Grindall <tim.grindall@gmail.com>
2018-03-06 17:07:47 -0800

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-	Todo.find().then(() => {
+	Todo.find().then((todos) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
