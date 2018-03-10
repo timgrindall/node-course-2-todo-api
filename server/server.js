9cd9ef1bfab7f8718aee2dbc8ab1cef68534b760
Timothy Grindall <tim.grindall@gmail.com>
2018-03-10 14:00:41 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var app = express();
 
@@ -29,6 +30,32 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	})
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	// Valid is using isValid
+	if (!ObjectId.isValid(id)) {
+		// 404 - send back empty send
+		return res.status(404).send();	//forgot return statement
+	}
+
+	// findById
+	Todo.findById(id).then((todo) => { // called it user instead of todo
+		// success
+		if (todo) {
+			// if todo - send it back
+			res.send(todo);
+		} else if (!todo) {
+			// if no todo - send back 404 with empty body
+			res.status(404).send();
+		}
+	}, (e) => {
+		// error
+		res.status(400).send(); // note: do not send error object because it may have sensitive information
+			// 400 - and send empty body back
+	});
 });
 
 app.listen(3000, () => {
