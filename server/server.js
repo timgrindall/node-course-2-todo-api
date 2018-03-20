49d77617c06c0ef7999a1112b9bb7c53a01bd462
Timothy Grindall <tim.grindall@gmail.com>
2018-03-20 16:56:01 -0700

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -114,6 +114,35 @@ app.patch('/todos/:id', (req, res) => {
 	});
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		console.log('something happened (400)!');
+		console.log(e);
+		res.status(400).send(e);
+	});
+});
+
+// GET /users
+app.get('/users', (req, res) => {
+	User.find().then((users) => res.send({users}), (e) => res.status(400).send(e));
+})
+
+// app.get('/todos', (req, res) => {
+// 	Todo.find().then((todos) => {
+// 		res.send({todos});
+// 	}, (e) => {
+// 		res.status(400).send(e);
+// 	})
+// });
+
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
 });
