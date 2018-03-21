037c5c474fd7530402a701e0d68ac5e585b30bd2
Timothy Grindall <tim.grindall@gmail.com>
2018-03-20 18:25:58 -0700

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -68,6 +69,26 @@ UserSchema.statics.findByToken = function (token) {
 		'tokens.access': 'auth'
 	});
 };
+
+UserSchema.pre('save', function (next) {
+	var user = this;
+
+	if (user.isModified('password')) {
+		//user.password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			})
+		});
+		//user.password = hashed value
+		//next();
+
+		//will have to call gensalt and hash functions
+	} else {
+		next();
+	}
+});
 
 var User = mongoose.model('User', UserSchema);
 
